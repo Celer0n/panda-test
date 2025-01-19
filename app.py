a6aa6b39c052a880f2de651510fad2ee4328fbc2
Celer0n <bobrovvartemm@gmail.com>
2025-01-19 17:11:02 +0200

    Test with deploy and monitoring 5

--- app.py
+++ app.py
@@ -1,24 +1,17 @@
 from flask import Flask
-from prometheus_client import Counter, Gauge, Histogram
 from prometheus_client import make_wsgi_app
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
 app = Flask(__name__)
 
-# Метрики
-REQUEST_COUNT = Counter('http_requests_total', 'Total HTTP requests', ['method', 'endpoint'])
-REQUEST_LATENCY = Histogram('http_request_duration_seconds', 'Request latency')
-
-@app.route("/")
-def hello():
-    REQUEST_COUNT.labels(method='GET', endpoint='/').inc()
-    with REQUEST_LATENCY.time():
-        return "Hello, Prometheus!"
-
-# Подключение Prometheus метрик
+# Подключение метрик для Prometheus
 app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
     '/metrics': make_wsgi_app()
 })
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
